64fa3b345a261b26d94c6acfcd2687c94da60db8
ajcanlas95 <aaronjohncanlas@gmail.com>
2017-12-26 07:50:18 +0800

    Added registration add function

---
+++ backend/regadd.php
@@ -0,0 +1,28 @@
+<?php
+
+include "database.php";
+
+$name = test_input($_GET["name"]);
+$email= test_input($_GET["email"]);
+$cnum= test_input($_GET["cnum"]);
+$reg_type= test_input($_GET["reg_type"]);
+
+
+$sql = 'INSERT INTO `registration`( `name`, `email`, `cnum`, `reg_type`) VALUES ("","","","")';
+
+if (mysqli_query($conn, $sql)) {
+    $msg="Registration Successful";
+} else {
+    $msg="Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+echo json_encode($msg);
+
+?>
