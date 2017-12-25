1e8fdbf7e272044aa6ad5f0ded25cfa11500d17b
ajcanlas95 <aaronjohncanlas@gmail.com>
2017-12-25 20:32:58 +0800

    Semicolon

--- backend/regnum.php
+++ backend/regnum.php
@@ -7,7 +7,7 @@
   if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        $msg=$row["Count (*)"]
+        $msg=$row["Count (*)"];
     }
     } else {
       $msg="0"
