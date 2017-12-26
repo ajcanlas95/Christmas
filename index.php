7eddc9ac0462529c546deba5f834e5e24e070931
bubuguduystunstugudunstuy <jomillematildo@ymail.com>
2017-12-26 22:18:47 +0800

    modal test part two

--- index.php
+++ index.php
@@ -196,15 +196,21 @@ include './backend/regnum.php';
       </div
     </section>
 
-    <div class="modal fade">
+    <!-- Button trigger modal -->
+    <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+      Launch demo modal
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-            <h4 class="modal-title">Modal title</h4>
+            <h4 class="modal-title" id="myModalLabel">Modal title</h4>
           </div>
           <div class="modal-body">
-            <p>One fine body&hellip;</p>
+            ...
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
