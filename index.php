84fd3945b918fee01385c8f05f41e073656d39d7
bubuguduystunstugudunstuy <jomillematildo@ymail.com>
2017-12-28 23:13:34 +0800

    Migrated the Navbar and Jumbotron. pending alignment issues for icons.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php
+<!-- <?php
 include './backend/regnum.php';
-?>
+?> -->
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -13,42 +13,49 @@ include './backend/regnum.php';
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <!-- Fontawesome CSS -->
+    <link href="vendor/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet">
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container">
-        <a class="navbar-brand" href="#">AYLC AT 20</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="#">AYLC</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">AYLA</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link " href="#">Venue</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link " href="#">Register</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link " href="#">Sponsors and Partners</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link " href="#">FAQs</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link " href="#">Contact</a>
-            </li>
-          </ul>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+      <div class="container justify-content-between">
+        <div class="col-2">
+          <a class="navbar-brand" href="#">AYLC AT 20</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
         </div>
+        <div class="col-7">
+          <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <div class="navbar-nav">
+              <a class="nav-item nav-link" href="#">AYLC</a>
+              <a class="nav-item nav-link" href="#">AYLA</a>
+              <a class="nav-item nav-link" href="#">Venue</a>
+              <a class="nav-item nav-link" href="#">Register</a>
+              <a class="nav-item nav-link" href="#">Sponsors and Partners</a>
+              <a class="nav-item nav-link" href="#">FAQs</a>
+              <a class="nav-item nav-link" href="#">Contact</a>
+            </div>
+          </div>
+        </div>
+
+        
       </div>
     </nav>
+    <div class="jumbotron">
+        <div class="container">
+          <h1 class="display-3 text-center">Be part with 4 others Coming</h1>
+          <p class="lead text-center">24 February 2018 | 3:00PM<br/>Globe Circuit Event Grounds, Makati</p>
+        </div>
+    </div>
+
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-1">
+          <button type="button" class="btn btn-link"><i class="fa fa-angle-double-down animated"></i></button>
+        </div>
+      </div>
+    </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
